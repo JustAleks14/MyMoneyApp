726a946c2bc837aad0f259eaba4ae7160424c1d4
A_Matrix <61456881+JustAleks14@users.noreply.github.com>
2020-05-31 09:34:50 +0300

    Add files via upload

---
+++ StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Forms;
+using MyMoney.View.SignForms;
+
+namespace MyMoney
+{
+    // Стартира приложението със формата за вход
+    static class StartUp
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SigninForm());
+        }
+    }
+}
